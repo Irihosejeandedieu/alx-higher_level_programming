7cf3e02f6dd1d48e9c0afbf3a9252ad8db688468
irihose jean de dieu <irhosejd2023@gmail.com>
2024-04-02 16:29:46 +0300

    correction

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,10 +4,9 @@ def safe_print_list(my_list=[], x=0):
     num = 0
     for j in range(x):
         try:
-            print("{}".format(my_list=[j]), end="")
+            print("{}".format(my_list[j]), end="")
             num = num + 1
         except IndexError:
             continue
     print("")
     return num
-
