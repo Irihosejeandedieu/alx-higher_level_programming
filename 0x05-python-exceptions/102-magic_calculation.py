01583b8470693d675da60292d818f4df0f5ea017
irihose jean de dieu <irhosejd2023@gmail.com>
2024-04-02 16:53:13 +0300

    102-magic_calculation.py

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-
 def magic_calculation(a, b):
-    count = 0
-    for j in range(1, 3):
+    result = 0
+    for i in range(1, 3):
         try:
-            if j > a:
+            if i > a:
                 raise Exception("Too far")
-            count += (a ** b) / j
+            result += (a ** b) / i
         except Exception:
-            count = b + a
+            result = b + a
             break
-    return count
-
+    return result
