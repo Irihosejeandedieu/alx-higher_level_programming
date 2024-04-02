fa091ecd7a1f0d3bb564921735d47b0aaca9aea8
irihose jean de dieu <irhosejd2023@gmail.com>
2024-04-02 16:06:44 +0300

    raise part!

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,19 @@
+def list_division(my_list_1, my_list_2, list_length):
+    num = 0
+    j = 0
+    n_list = []
+    for j in range(list_length):
+        try:
+            r = my_list_1[j] / my_list_2[j]
+        except TypeError:
+            print("wrong type")
+            r = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            r = 0
+        except IndexError:
+            print("out of range")
+            r = 0
+        finally:
+            n_list.append(r)
+    return n_list
