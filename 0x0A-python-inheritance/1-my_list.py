4dfc20967466df70dd68ca66ed13d51b7d16b93a
irihose jean de dieu <irhosejd2023@gmail.com>
2024-04-15 21:22:08 +0300

    1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+class MyList:
+    """creating a method to print a sorted list"""
+    pass
+
+    def print_sorted(self):
+    """this one is going to print the list, but sorted (ascending sort)"""
+
+        print(sorted(list(self)))
